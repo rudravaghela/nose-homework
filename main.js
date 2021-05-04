be5c2ac316c64cb4f41f0e50c77258b59cec010e
rudravaghela <72458643+rudravaghela@users.noreply.github.com>
2021-05-04 19:46:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+nose_X = 0;
+nose_Y = 0;
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+
+}
+
+function draw(){
+
+} 
+
+function take_snapshot(){
+    save("rudra.png");
+}
